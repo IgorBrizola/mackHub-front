311fbc1bf6f3887cc2a4fe34c625c954e0870d9c
João Henrique <167344636+riquezlvs@users.noreply.github.com>
2025-10-01 00:01:11 +0000

    feat: aprimora layout e interatividade do menu e agenda

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("dropdownDefaultButton");
-    const dropdown = document.getElementById("dropdown");
-
-    button.addEventListener("click", () => {
-      dropdown.classList.toggle("hidden");
-    });
-
-  document.addEventListener("click", (event) => {
-      if (!button.contains(event.target) && !dropdown.contains(event.target)) {
-        dropdown.classList.add("hidden");
-      }
-    });
-});
-
+const buttons = document.querySelectorAll('.menu-button');
 const sectionAgenda = document.getElementById("Agenda");
 
 let eventosAgenda = [
@@ -27,16 +13,50 @@ document.addEventListener("DOMContentLoaded", () => {
     <div class="h-32 w-full flex justify-between items-center border border-[#B01C1C] rounded-lg">
       <div class="mx-10">
           <div class="flex items-center gap-2">
-              <h2 class="font-bold">${evento.titulo}</h2> 
-              <span class="bg-red-500 rounded-xl px-2 text-white font-medium text-sm">${evento.tipo}</span>
+              <h2 class="font-bold text-lg">${evento.titulo}</h2> 
+              <span class="bg-[#B01C1C] rounded-xl px-2 text-white font-medium text-sm">${evento.tipo}</span>
           </div>
-          <p>${evento.componente}</p>
-          <span>${evento.data}</span>
+          <p class="text-gray-500">${evento.componente}</p>
+          <span class="text-gray-500">${evento.data}</span>
       </div>
       <div class="mx-10">
-          <button class="p-2 rounded-lg border border-gray-300"><i class="fa-regular fa-bell mx-1"></i><span>Lembrete</span></button>
+          <button class="p-2 rounded-lg border border-gray-300 font-medium text-sm"><i class="fa-regular fa-bell mx-1"></i><span>Lembrete</span></button>
       </div>
     </div>`; 
     sectionAgenda.innerHTML += sectionAgendacontent;
   });
+
+  buttons.forEach(btn => btn.classList.remove('bg-white'));
+
+  buttons[0].classList.add('bg-white');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('bg-white'));
+      button.classList.add('bg-white');
+      });
+    });
+
+
+
+
+  buttons.forEach(btn => {
+    btn.classList.remove('text-black');
+    btn.classList.add('text-gray-400');
+  });
+
+  buttons[0].classList.remove('text-gray-400');
+  buttons[0].classList.add('text-black');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => {
+        btn.classList.remove('text-black');
+        btn.classList.add('text-gray-400');
+      });
+      button.classList.remove('text-gray-400');
+      button.classList.add('text-black');
+    });
+  });
 });
+
